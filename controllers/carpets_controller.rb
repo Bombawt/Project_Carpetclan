a14d0bc70aa65d41173fe0ba5e930f83be410618
Kira Marston <kiramarston@gmail.com>
2020-04-06 16:22:07 +0100

    Added edit.erb files to each views directory, added Edit functionality to Carpets.

--- controllers/carpets_controller.rb
+++ controllers/carpets_controller.rb
@@ -29,3 +29,16 @@ get '/carpets/:id' do
   @suppliers = Supplier.all()
   erb(:"carpets/show")
 end
+
+get '/carpets/:id/edit' do
+  id = params['id'].to_i()
+  @carpets = Carpet.find(id)
+  @suppliers = Supplier.all()
+  erb(:"carpets/edit")
+end
+
+post '/carpets/:id' do
+  carpet = Carpet.new(params)
+  carpet.update
+  redirect('/carpets/' + params['id'])
+end
